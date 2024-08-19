4e1cb2a4659e9980d5c95b9c75794804a52b9a00
Igor Eulalio <igor.eulalio@sysdig.com>
2024-08-19 11:55:12 -0300

    tf fmt

--- kubernetes/crossplane.tf
+++ kubernetes/crossplane.tf
@@ -5,13 +5,13 @@
 #     chart            = "crossplane"
 #     version          = "1.16.0"
 #     create_namespace = true
-    
+
 #     values = [
 #         templatefile("${path.module}/values/crossplane.yaml", {
 #         cluster_name     = local.name
 #         }),
 #     ]
-    
+
 #     depends_on = [azurerm_kubernetes_cluster.aks]
-  
+
 # }
--- kubernetes/prometheus.tf
+++ kubernetes/prometheus.tf
@@ -1,15 +1,14 @@
 resource "helm_release" "prometheus" {
-    name      = "prometheus"
-    namespace = "monitoring"
-    repository       = "https://prometheus-community.github.io/helm-charts"
-    chart            = "kube-prometheus-stack"
-    version          = "61.6.0"
-    create_namespace = true
-    
-    values = [
-        file("${path.module}/values/prometheus.yaml"),
-    ]
-    
-    depends_on = [azurerm_kubernetes_cluster.aks]
-  
+  name             = "prometheus"
+  namespace        = "monitoring"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  chart            = "kube-prometheus-stack"
+  version          = "61.6.0"
+  create_namespace = true
+
+  values = [
+    file("${path.module}/values/prometheus.yaml"),
+  ]
+
+  depends_on = [azurerm_kubernetes_cluster.aks]
 }
